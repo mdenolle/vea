0430b10f288994586340e0bcaa856571cc80d680
mdenolle <mdenolle@fas.harvard.edu>
2017-02-07 13:44:49 -0500

    Add files via upload

--- sub/irf2dc.m
+++ sub/irf2dc.m
@@ -1,7 +1,9 @@
 % function from deep irf to DC:
-function  [R,T,Z]=irf2dc(M,FT,bigC,ZZ,ZR,RZ,RR,TT,HS,az,Sh,dr)
+function  [R,T,Z]=irf2dc(M,FT,bigC,ZZ,ZR,RZ,RR,TT,HS,az,Sh,dr,typ)
+% function  [R,T,Z,corrR,corrT,corrZ]=irf2dc(M,FT,bigC,ZZ,ZR,RZ,RR,TT,HS,az,Sh,dr,typ)
 % remember the conventions, R = radial, T transverse, Z vertical downward.
-
+% if typ='f', provide results in frequency domain
+% if typ = 't', provide results in time domain
 % rotate the moment tensor at each point source.
 for ir=1:length(ZZ)
     cosa=cos(az{ir}*pi/180);cosb=cosa;
@@ -19,65 +21,49 @@ for ir=1:length(ZZ)
 %     M2{ir}(3,3) = M(3,3);
     M2{ir} = R1*M*R2';
 end
-
 for ifreq=2:length(bigC)
     if isempty(bigC(ifreq).a);continue;end
     C = bigC(ifreq).a;
+    kr(ifreq)=C(1).kr(1);
+    kl(ifreq)=C(1).kl(1);
     [~,ib]=unique(C(1).zz);
-    Nmode = min(min(size(C(1).Dux)),min(min(size(C(1).ux))));
-    if Nmode==1;
-        imode=1;
-        ux0(imode)=C(1).ux(end);uz0(imode)=C(1).uz(end);uy0(imode)=C(1).uy(end);
-        uxi(imode) = interp1(C(1).zz(ib),C(1).ux(ib),HS,'linear');
-        uzi(imode) = interp1(C(1).zz(ib),C(1).uz(ib),HS,'linear');
-        duxi(imode) = -interp1(C(1).zz(ib),C(1).Dux(ib),HS,'linear');
-        duzi(imode) = -interp1(C(1).zz(ib),C(1).Duz(ib),HS,'linear');
-        uyi(imode) = interp1(C(1).zz(ib),C(1).uy(ib),HS,'linear');
-        duyi(imode) = -interp1(C(1).zz(ib),C(1).Duy(ib),HS,'linear');
-    else
-        for imode=1:Nmode
-            uxi(imode) = interp1(C(1).zz(ib),C(1).ux(ib,imode),HS,'linear');
-            uyi(imode) = interp1(C(1).zz(ib),C(1).uy(ib,imode),HS,'linear');
-            uzi(imode) = interp1(C(1).zz(ib),C(1).uz(ib,imode),HS,'linear');
-            duxi(imode) = -interp1(C(1).zz(ib),C(1).Dux(ib,imode),HS,'linear');
-            duyi(imode) = -interp1(C(1).zz(ib),C(1).Duy(ib,imode),HS,'linear');
-            duzi(imode) = -interp1(C(1).zz(ib),C(1).Duz(ib,imode),HS,'linear');
-            ux0(imode)=C(1).ux(end,imode);uz0(imode)=C(1).uz(end,imode);
-            uy0(imode)=C(1).uy(end,imode);
-        end
-    end
-    for ir=1:length(ZZ)  
-        if isempty(ZZ{ir});continue;end
-        NmodeR = min(Nmode,min(size(ZZ{ir})));
-        NmodeL = min(Nmode,min(size(TT{ir})));
-        Th{ir}(ifreq) =0;
-        Rh{ir}(ifreq) =0;
-        Zh{ir}(ifreq) =0;
-        
-        for imode=1%:NmodeL
-            DT(imode)=dr{ir}/C(1).cl(imode);
-            Th{ir}(ifreq) =  Th{ir}(ifreq) +( 1i*C(1).kl(imode)*M2{ir}(2,1)*uyi(imode)/uy0(imode) ...
-                + duyi(imode)/uy0(imode)*M2{ir}(2,3)   )*TT{ir}(imode,ifreq).*exp(-1i*C(1).omega*DT(imode));
-        end
-        for imode=1%1:NmodeR
-            DT(imode)=dr{ir}/C(1).cr(imode);
-            Rh{ir}(ifreq) =  Rh{ir}(ifreq)+ ((1i*C(1).kr(imode)*M2{ir}(3,1)*uzi(imode)/uz0(imode) + ...
-                duzi(imode)/uz0(imode)*M2{ir}(3,3) )*RZ{ir}(imode,ifreq) ...
-                    + ( 1i*C(1).kr(imode)*M2{ir}(1,1)*uxi(imode)/ux0(imode) + ...
-                    duxi(imode)/ux0(imode)*M2{ir}(1,3) )*RR{ir}(imode,ifreq) ).*exp(-1i*C(1).omega*DT(imode) );
-            Zh{ir}(ifreq) = Zh{ir}(ifreq)+  ( ( 1i*C(1).kr(imode)*M2{ir}(1,1)*uxi(imode)/ux0(imode) + ...
-                duxi(imode)/ux0(imode)*M2{ir}(1,3) )*ZR{ir}(imode,ifreq)  ...
-                    + ( 1i*C(1).kr(imode)*M2{ir}(3,1)*uzi(imode)/uz0(imode) + ...
-                    duzi(imode)/uz0(imode)*M2{ir}(3,3) )*ZZ{ir}(imode,ifreq) ).*exp(-1i*C(1).omega*DT(imode) ) ;
-                
-        end
-        Th{ir}(ifreq) = Th{ir}(ifreq)*Sh(ifreq)*1E-3;
-        Rh{ir}(ifreq) = Rh{ir}(ifreq)*Sh(ifreq)*1E-3;
-        Zh{ir}(ifreq) = Zh{ir}(ifreq)*Sh(ifreq)*1E-3;
-    end
+    omega(ifreq)=C(1).omega;
+%     Nmode = min(min(size(C(1).Dux)),min(min(size(C(1).ux))));
+    
+%     if Nmode==1;
+        ux0(ifreq)=C(1).ux(end);%uz0(ifreq)=C(1).uz(end);uy0(ifreq)=C(1).uy(end);
+        uxi(ifreq) = interp1(C(1).zz(ib),C(1).ux(ib),HS,'linear');
+        uzi(ifreq) = interp1(C(1).zz(ib),C(1).uz(ib),HS,'linear');
+        duxi(ifreq) = -interp1(C(1).zz(ib),C(1).Dux(ib),HS,'linear');
+        duzi(ifreq) = -interp1(C(1).zz(ib),C(1).Duz(ib),HS,'linear');
+        uyi(ifreq) = interp1(C(1).zz(ib),C(1).uy(ib),HS,'linear');
+        duyi(ifreq) = -interp1(C(1).zz(ib),C(1).Duy(ib),HS,'linear');
 end
+uy0=1;
+uz0=1;
+for ir=1:length(ZZ)  
+    if isempty(ZZ{ir});continue;end
+    
+        Th{ir}(2:ifreq) =0;
+        Rh{ir}(2:ifreq) =0;
+        Zh{ir}(2:ifreq) =0;
+        Th{ir}(2:ifreq) =  ( 1i*kl(2:ifreq).*M2{ir}(2,1).*uyi(2:ifreq)./uy0 ...
+                + duyi(2:ifreq)./uy0*M2{ir}(2,3) ).*TT{ir}(2:ifreq).*exp(-1i.*dr{ir}.*kl(2:ifreq));
+        Rh{ir}(2:ifreq) = ((1i*kr(2:ifreq)*M2{ir}(3,1).*uzi(2:ifreq)./uz0 + ...
+                duzi(2:ifreq)./uz0*M2{ir}(3,3) ).*RZ{ir}(2:ifreq) ...
+                + ( 1i*kr(2:ifreq)*M2{ir}(1,1).*uxi(2:ifreq)./ux0(2:ifreq) + ...
+                duxi(2:ifreq)./ux0(2:ifreq)*M2{ir}(1,3) ).*RR{ir}(2:ifreq) ).*exp(-1i.*dr{ir}.*kr(2:ifreq));
+        Zh{ir}(2:ifreq) = (( 1i*kr(2:ifreq).*M2{ir}(1,1).*uxi(2:ifreq)./ux0(2:ifreq) + ...
+                duxi(2:ifreq)./ux0(2:ifreq)*M2{ir}(1,3) ).*ZR{ir}(2:ifreq)  ...
+                + ( 1i*kr(2:ifreq)*M2{ir}(3,1).*uzi(2:ifreq)/uz0 + ...
+                duzi(2:ifreq)/uz0*M2{ir}(3,3) ).*ZZ{ir}(2:ifreq) ).*exp(-1i.*dr{ir}.*kr(2:ifreq)) ;
 
+end
+if strcmp(typ,'t')==1
 for ir=1:length(ZZ)
+Th{ir}(1:ifreq-1) = Th{ir}(1:ifreq-1).*Sh(1:ifreq-1)*1E-3;
+Rh{ir}(1:ifreq-1) = Rh{ir}(1:ifreq-1).*Sh(1:ifreq-1)*1E-3;
+Zh{ir}(1:ifreq-1) = Zh{ir}(1:ifreq-1).*Sh(1:ifreq-1)*1E-3;
 Th{ir}(ifreq:FT.nwin)=0;Th{ir}(1)=0;
 T{ir}  = (ifft(Th{ir},'symmetric'));
 
@@ -86,4 +72,8 @@ R{ir}  = (ifft(Rh{ir},'symmetric'));
 
 Zh{ir}(ifreq:FT.nwin)=0;Zh{ir}(1)=0;
 Z{ir}  = (ifft(Zh{ir},'symmetric'));
+end
+
+else
+    T=Th;R=Rh;Z=Zh;
 end
